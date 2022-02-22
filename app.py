cbeaeb932cf6238ec604ce7d20c56eeb2438d15a
HENRY Maxime FRSS <58228792+maxime-henry@users.noreply.github.com>
2022-02-22 09:46:37 +0100

    Update of the script for PT

--- app.py
+++ app.py
@@ -3,65 +3,54 @@ import pandas as pd
 import plotly.express as px
 from statistics import mean
 import datetime
-# import boto3
-# from getWeather import get_weather
 
-st.set_page_config(layout="wide")
 
+st.set_page_config(layout="wide") # Wide page 
+
+# Two columns for wtitle and image 
 col1, col2 = st.columns(2)
-
-# s3 = boto3.resource(
-#     service_name='s3',
-#     region_name='eu-west-3',
-#     aws_access_key_id=st.secrets.db_credentials.aws_access_key_id,
-#     aws_secret_access_key=st.secrets.db_credentials.aws_secret_access_key
-# )
 
 with col1:
     st.title('Weather app')
 
 with col2:
-    st.image("Logo_SPR.jpg", width=150)
+    st.image("data\Logo_SPR.jpg", width=150)
 
 
 # country choice 
 
-selected_country = st.sidebar.selectbox("Select a country", options=['TR','FR','ES','HU','UA' ], help = "Select the coutry you need the data from")
+selected_country = st.sidebar.selectbox("Select a country", options=['TR','PT' ], help = "Select the coutry you need the data from")
 
 @st.cache(max_entries = 1)
+# Only working for turkey now
 def load_data(country):
-    # obj = s3.Bucket('weatherdataspr').Object(country +'.csv').get()
-    # data = pd.read_csv(obj['Body'], index_col=0)
-    # data = pd.read_csv("TR.csv")
-    # data = pd.read_csv('summarizedfebruary.csv')
-    data = pd.read_csv("summarizedfebruary.zip")
+
+    data = pd.read_csv(f"data\{country}.zip")
+    # data = pd.read_csv("PT.csv")
     # data = get_weather(country)
     data['year'] = 2020
     data['DATE'] = pd.to_datetime(data[['day', 'month','year']], dayfirst=True).dt.date
-    
-    # 
-    
     return data
 
 # Load the data ####################
-
 data = load_data(country = selected_country)
 
 
 
 # Col names and selection of the good variable
-col = data.columns.values.tolist()
 col = ['temperature_c_2_m_above_gnd_max',
                             'temperature_c_2_m_above_gnd_min',
                             'temperature_c_2_m_above_gnd_avg',
                             'precipitation_total_grid_mm_surface_sum',
                             'wind_speed_km_per_h_2_m_above_gnd_avg',
-                            'relative_humidity_pct_2_m_above_gnd_avg',
-                            # 'shortwave_radiation_w_per_m2_surface_sum'
-                            'GDD'
+                            'relative_humidity_pct_2_m_above_gnd_avg'
+                            # 'GDD'
                             ]
-variable_choice = st.sidebar.selectbox('Selection of the variable', col, help = "Select the weather variable that you want to be displayed on the map")
 
+
+# Select the weather variables
+variable_choice = st.sidebar.selectbox('Selection of the variable', col, help = "Select the weather variable that you want to be displayed on the map")
+# Select the aggregation 
 aggregation = st.sidebar.radio("Slect the aggregation", ("Average", 'Sum'))
 
 #### DATE Selection ####################""
@@ -77,14 +66,14 @@ end_time =  st.sidebar.date_input(label= "End date", value=datetime.date(2020,9,
 def do_the_aggregation(data, start,end,variable, aggregation):
     ### Filtering #########################################
     print(variable)
-
-    if variable == 'GDD':
-        datatemp = pd.read_csv('GDD.csv')
-        datatemp['year'] = 2020
-        datatemp['DATE'] = pd.to_datetime(datatemp[['day', 'month','year']], dayfirst=True).dt.date
-        temp = datatemp.loc[(datatemp['DATE']>= start) & (datatemp['DATE']<= end)]
-    else:
-        temp = data.loc[(data['DATE']>= start) & (data['DATE']<= end)] 
+    # GDD data not in the zip file so imported here 
+    # if variable == 'GDD':
+    #     datatemp = pd.read_csv('GDD.csv')
+    #     datatemp['year'] = 2020
+    #     datatemp['DATE'] = pd.to_datetime(datatemp[['day', 'month','year']], dayfirst=True).dt.date
+    #     temp = datatemp.loc[(datatemp['DATE']>= start) & (datatemp['DATE']<= end)]
+    # else:
+    temp = data.loc[(data['DATE']>= start) & (data['DATE']<= end)] 
 
     temp=temp[['lat','lon', variable]]
     temp[variable] = temp[variable].astype(float).round(2)
@@ -93,13 +82,14 @@ def do_the_aggregation(data, start,end,variable, aggregation):
         aggreg = temp.groupby(['lat', 'lon']).aggregate('mean').reset_index()
     if aggregation == 'Sum':
         aggreg = temp.groupby(['lat', 'lon']).aggregate('sum').reset_index()
-    
     return(aggreg)
 
 
+# Do the aggregation 
 aggreg = do_the_aggregation(data, start= start_time, end = end_time, variable = variable_choice, aggregation=aggregation)
 
 
+# Create the map with mapbox ans poltly express
 px.set_mapbox_access_token(open(".mapbox_token").read())
 
 fig = px.scatter_mapbox(aggreg, lat=aggreg.lat, lon=aggreg.lon, color=round(aggreg[variable_choice],2), size=round(abs(aggreg[variable_choice]),2),
@@ -115,7 +105,9 @@ fig.update_layout(mapbox_style="open-street-map",
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, height=800)
 
 
-st.header('Map of '+ variable_choice )
 
+# Display header
+st.header('Map of '+ variable_choice )
+# Dispay map 
 st.plotly_chart(fig, use_container_width=True)
 
